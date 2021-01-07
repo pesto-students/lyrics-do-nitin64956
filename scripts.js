36b91196c98f37f808675723c98d17426faddff5
nitin64956 <nitin64956@gmail.com>
2021-01-07 17:30:28 +0530

    Added More features and functionality to app

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 const searchBar = document.getElementById('searchBar');
 const lyricsDiv = document.getElementById('lyrics-div');
 
-
 let inputValue = "";
 let displaySongs = {};
 
@@ -10,7 +9,9 @@ searchBar.addEventListener('change',() => {
     inputValue = searchBar.value;
 });
 
+
 let searchButton = document.getElementById('searchButton');
+
 
 //function to get lyrics for a title and artist name and to append the paragraph in DOM
 function getLyrics(artist,title,tag,btn) {
@@ -33,6 +34,12 @@ function getLyrics(artist,title,tag,btn) {
     });
 }
 
+function hideLyrics(div) {
+    if (div && div.nextSibling.nodeName == "P") {
+        div.parentNode.removeChild(div.nextSibling)
+    }
+}
+
 //fn to clear old records from dom
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
@@ -41,11 +48,12 @@ function removeAllChildNodes(parent) {
 }
 
 // fn to load the next page data
-function nextPage (link) {
+function changePage (link) {
     fetch(link)
         .then(response => response.json())
         .then(json => 
             {
+                
                 displaySongs = json;
                 createList(displaySongs);
             });
@@ -56,21 +64,22 @@ function createList (displayResponse) {
     const oldLyrics = document.querySelector('#lyrics-div');
                       removeAllChildNodes(oldLyrics);
     let songsData = displaySongs.data;
-    let totalResults = displaySongs.total;
     let nextLink = displaySongs.next;
+    let prevLink = displaySongs.prev;
 
-    if(!songsData) {
-        let para = document.createElement("P");
-        let emptyMessage = document.createTextNode(`No Records Found, Please Search Again`);
-        para.appendChild(emptyMessage);
-        lyricsDiv.appendChild(para);
-        return;
-
-    }
+    
 
     for (let i = 0;i < songsData.length;i++) {
         let element = songsData[i];
         let div = document.createElement("div");
+        let image = document.createElement("img");
+            image.src = element.album.cover_small;
+        let buttonsDiv = document.createElement("div");
+            buttonsDiv.className = "div-buttons";
+        let audioElem = document.createElement("audio");
+                        audioElem.src = element.preview;
+        div.appendChild(audioElem)
+
         let para = document.createElement("P");
         let text = document.createTextNode(`${element.artist.name} - ${element.title}`);
         para.appendChild(text);
@@ -80,30 +89,68 @@ function createList (displayResponse) {
         button.className = "lyricsButton";
         button.addEventListener('click',() => 
         { 
-        
-            let lyrics = {}; 
-            if (lyrics[element.id]) {
-                return lyrics[element.id];
-            }
-            else {
-                lyrics[element.id] = getLyrics(element.artist.name,element.title,div,button);
-                return lyrics[element.id];
-            }
+            getLyrics(element.artist.name,element.title,div,button);
         });
 
+        let closeButton = document.createElement("button");
+        closeButton.textContent = "Hide Lyrics";
+        closeButton.className = "lyricsButton";
+        closeButton.addEventListener('click',() => 
+        { 
+            hideLyrics(div);
+        });
+
+        let playButton = document.createElement("button");
+        playButton.textContent = "Play";
+        playButton.className = "mediaButton";
+        playButton.addEventListener('click',() => 
+        { 
+           audioElem.play();
+        });
+
+        let pauseButton = document.createElement("button");
+        pauseButton.textContent = "Pause";
+        pauseButton.className = "mediaButton";
+        pauseButton.addEventListener('click',() => 
+        { 
+            audioElem.pause();
+        });
+
+
+        buttonsDiv.appendChild(playButton);
+        buttonsDiv.appendChild(pauseButton);
+        buttonsDiv.appendChild(button);
+        buttonsDiv.appendChild(closeButton);
+
+        div.appendChild(image);
         div.appendChild(para);
-        div.appendChild(button);
+        div.appendChild(buttonsDiv);
+
         div.className = "recordDiv";
 
         lyricsDiv.appendChild(div);
     };
+
+    if (prevLink) {
+        var prevButton = document.createElement("button");
+        prevButton.textContent = "Previous";
+        prevButton.className = "pageButton";
+        prevButton.style.marginTop = "10px";
+        prevButton.addEventListener('click',() => {
+            changePage(`https://cors-anywhere.herokuapp.com/${prevLink}`);
+        })
+        lyricsDiv.appendChild(prevButton);
+
+    }
+
+
     if (nextLink) {
         var nextButton = document.createElement("button");
         nextButton.textContent = "Next";
-        nextButton.className = "lyricsButton";
+        nextButton.className = "pageButton";
         nextButton.style.marginTop = "10px";
         nextButton.addEventListener('click',() => {
-            nextPage(`https://cors-anywhere.herokuapp.com/${nextLink}`);
+            changePage(`https://cors-anywhere.herokuapp.com/${nextLink}`);
         })
         lyricsDiv.appendChild(nextButton);
 
